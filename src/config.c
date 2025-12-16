a1530f47332b4f92904f72c1fbc92bbc30e8048a
Manuel <manuelserrano004@gmail.com>
2025-12-16 10:37:53 +0100

    Aggiornamento robustezza erogatore ticket e config

--- src/config.c
+++ src/config.c
@@ -4,28 +4,47 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
+#include <ctype.h>
+
+static void safe_copy_trim(char* dest, const char* src, size_t n, size_t dest_sz) {
+    if (n >= dest_sz) n = dest_sz - 1;
+    
+    strncpy(dest, src, n);
+    dest[n] = '\0'; 
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (isspace((unsigned char)dest[i])) dest[i] = '\0';
+        else break;
+    }
+}
 
 static int parse_line_kv(const char* line, char* key, size_t ksz, char* val, size_t vsz) {
     const char* eq = strchr(line, '=');
     if (!eq) return -1;
+    
     size_t klen = (size_t)(eq - line);
-    while (klen && (line[klen-1] == ' ' || line[klen-1] == '\t')) klen--;
-    size_t vlen = strlen(eq + 1);
-    while (vlen && ((eq+1)[vlen-1] == '\n' || (eq+1)[vlen-1] == '\r' || (eq+1)[vlen-1] == ' ')) vlen--;
+    while (klen > 0 && isspace((unsigned char)line[klen-1])) klen--;
 
-    if (klen >= ksz || vlen >= vsz) return -1;
-    memcpy(key, line, klen); key[klen] = '\0';
-    memcpy(val, eq + 1, vlen); val[vlen] = '\0';
+    const char* vstart = eq + 1;
+    while (*vstart && isspace((unsigned char)*vstart) && *vstart != '\n') vstart++;
+    
+    safe_copy_trim(key, line, klen, ksz);
+    safe_copy_trim(val, vstart, strlen(vstart), vsz);
+
     return 0;
 }
 
 const char* risolvi_percorso_config(int argc, char** argv) {
+    const char* env_path = getenv("POSTE_CONFIG");
+    if (env_path && strlen(env_path) > 0) return env_path;
+
     for (int i = 1; i < argc; ++i) {
         if (strncmp(argv[i], "--config=", 9) == 0) {
             const char* p = argv[i] + 9;
             if (*p) return p;
         }
     }
+    
     return PERCORSO_FILE_CONFIG;
 }
 
@@ -47,41 +66,35 @@ void carica_configurazione(const char* percorso,
     int   d_day_duration     = 60;        
     int   d_n_requests       = 1;
     
-
-    FILE* f = fopen(percorso ? percorso : PERCORSO_FILE_CONFIG, "r");
+    const char* path = percorso ? percorso : PERCORSO_FILE_CONFIG;
+    FILE* f = fopen(path, "r");
+    
     if (f) {
+        printf("[Config] Caricamento da: %s\n", path);
         char line[256], k[128], v[128];
+        
         while (fgets(line, sizeof(line), f)) {
-            if (line[0] == '#' || line[0] == ';' || line[0] == '\n') continue;
+            char* p = line;
+            while (*p && isspace((unsigned char)*p)) p++;
+            if (*p == '#' || *p == ';' || *p == '\0') continue;
+
             if (parse_line_kv(line, k, sizeof(k), v, sizeof(v)) != 0) continue;
 
-            if (!strcasecmp(k, "NOF_WORKERS"))
-                d_num_workers = atoi(v);
-            else if (!strcasecmp(k, "NOF_USERS"))
-                d_num_users = atoi(v);
-            else if (!strcasecmp(k, "NOF_WORKER_SEATS"))
-                d_num_worker_seats = atoi(v);
-            else if (!strcasecmp(k, "SIM_DURATION"))
-                d_sim_duration = atoi(v);
-            else if (!strcasecmp(k, "N_NANO_SECS"))
-                d_n_nano_secs = atoi(v);
-            else if (!strcasecmp(k, "NOF_PAUSE"))
-                d_max_pause = atoi(v);
-            else if (!strcasecmp(k, "P_SERV_MIN"))
-                d_p_serv_min = (float)atof(v);
-            else if (!strcasecmp(k, "P_SERV_MAX"))
-                d_p_serv_max = (float)atof(v);
-            else if (!strcasecmp(k, "EXPLODE_THRESHOLD"))
-                d_explode_threshold = atoi(v);
-            else if (!strcasecmp(k, "DAY_DURATION"))
-                d_day_duration = atoi(v);
-            else if (!strcasecmp(k, "N_REQUESTS"))
-                d_n_requests = atoi(v);
+            if (!strcasecmp(k, "NOF_WORKERS"))        d_num_workers = atoi(v);
+            else if (!strcasecmp(k, "NOF_USERS"))     d_num_users = atoi(v);
+            else if (!strcasecmp(k, "NOF_WORKER_SEATS")) d_num_worker_seats = atoi(v);
+            else if (!strcasecmp(k, "SIM_DURATION"))  d_sim_duration = atoi(v);
+            else if (!strcasecmp(k, "N_NANO_SECS"))   d_n_nano_secs = atoi(v);
+            else if (!strcasecmp(k, "NOF_PAUSE"))     d_max_pause = atoi(v);
+            else if (!strcasecmp(k, "P_SERV_MIN"))    d_p_serv_min = (float)atof(v);
+            else if (!strcasecmp(k, "P_SERV_MAX"))    d_p_serv_max = (float)atof(v);
+            else if (!strcasecmp(k, "EXPLODE_THRESHOLD")) d_explode_threshold = atoi(v);
+            else if (!strcasecmp(k, "DAY_DURATION"))  d_day_duration = atoi(v);
+            else if (!strcasecmp(k, "N_REQUESTS"))    d_n_requests = atoi(v);
         }
         fclose(f);
     } else {
-        fprintf(stderr, "[carica_configurazione] Impossibile aprire %s, uso i valori predefiniti.\n",
-                percorso ? percorso : PERCORSO_FILE_CONFIG);
+        fprintf(stderr, "[Config] Impossibile aprire %s. Uso valori predefiniti.\n", path);
     }
 
     if (num_operatori)    *num_operatori    = d_num_workers;
@@ -95,5 +108,4 @@ void carica_configurazione(const char* percorso,
     if (soglia_attesa)    *soglia_attesa    = d_explode_threshold;
     if (durata_giorno)    *durata_giorno    = d_day_duration;
     if (num_richieste)    *num_richieste    = (d_n_requests < 1 ? 1 : d_n_requests);
-    
 }
--- src/erogatore_ticket.c
+++ src/erogatore_ticket.c
@@ -20,17 +20,22 @@ static void signal_handler(int sig) {
 }
 
 int main(void) {
-
     struct sigaction sa;
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
-    sigaction(SIGTERM, &sa, NULL);
-    sigaction(SIGINT, &sa, NULL);
+    
+    TEST_ERROR(sigaction(SIGTERM, &sa, NULL), "sigaction SIGTERM");
+    TEST_ERROR(sigaction(SIGINT, &sa, NULL), "sigaction SIGINT");
 
     int shmid = ottieni_memoria_condivisa();
+    if (shmid < 0) TEST_ERROR(-1, "shmget erogatore");
+
     SharedMemory* shm = collega_memoria_condivisa(shmid);
+    if (!shm) TEST_ERROR(-1, "shmat erogatore");
+
     int sem_id = ottieni_semafori();
+    if (sem_id < 0) TEST_ERROR(-1, "semget erogatore");
 
     int msgid  = shm->msgqueue_id;
 
@@ -38,76 +43,89 @@ int main(void) {
     shm->erogatore_inizializzato = 1;
     sem_post_operation(sem_id, SEM_MUTEX, 1);
 
-    printf("Erogatore ticket avviato. In attesa di richieste...\n");
+    printf("[Erogatore] Avviato (PID %d). In attesa di richieste...\n", getpid());
     
     sem_post_operation(sem_id, SEM_INIT_BARRIER, 1);
 
     while (shm->attivo && !termination_requested) {
         MessaggioRichiesta richiesta;
+        
         ssize_t n = msgrcv(msgid, &richiesta, sizeof(richiesta) - sizeof(long),
                            TIPO_MSG_RICHIESTA, 0);
+        
         if (n == -1) {
             if (errno == EINTR) {
                 if (termination_requested || !shm->attivo) break;
                 continue;
             }
-            if (!shm->attivo || termination_requested) break;
-            perror("erogatore_ticket msgrcv");
-            continue;
+            if (errno == EIDRM || errno == EINVAL) {
+                break;
+            }
+            perror("[Erogatore] msgrcv error");
+            break;
         }
 
         int servizio = richiesta.tipo_servizio;
+        
         if (servizio < 1 || servizio > NUM_SERVIZI) {
-            fprintf(stderr, "[Erogatore] Servizio %d non valido (PID=%d)\n", servizio, (int)richiesta.pid_utente);
+            fprintf(stderr, "[Erogatore] Warning: Servizio %d non valido (PID=%d)\n", 
+                    servizio, (int)richiesta.pid_utente);
             continue;
         }
+        
         int indice = servizio - 1;
-        int pid_u = (int)richiesta.pid_utente;
+        pid_t pid_u = (pid_t)richiesta.pid_utente;
         
-        int giorno_attuale, aperto, minuto_attuale;
+        int giorno_att, aperto, min_att, sportelli_disp;
+        
         sem_wait_operation(sem_id, SEM_MUTEX, 1);
-        giorno_attuale = shm->giorno;
+        giorno_att     = shm->giorno;
         aperto         = shm->giorno_aperto;
-        minuto_attuale = shm->minuto_corrente;
-        
-        int sportelli_disponibili = shm->sportelli_per_servizio_oggi[indice];
+        min_att        = shm->minuto_corrente;
+        sportelli_disp = shm->sportelli_per_servizio_oggi[indice];
         sem_post_operation(sem_id, SEM_MUTEX, 1);
 
         if (!aperto) {
-            invia_risposta(msgid, (pid_t)pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_attuale);
+            invia_risposta(msgid, pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_att);
             continue;
         }
         
-        if (sportelli_disponibili == 0) {            
+        if (sportelli_disp == 0) {            
             sem_wait_operation(sem_id, SEM_MUTEX, 1);
-            processa_utente_non_servito(shm, servizio);
+            shm->servizi_non_completati_oggi[indice]++;
+            shm->totale_servizi_non_completati[indice]++;
             sem_post_operation(sem_id, SEM_MUTEX, 1);
             
-            invia_risposta(msgid, (pid_t)pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_attuale);
+            invia_risposta(msgid, pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_att);
             continue;
         }
 
-        ElementoCoda e = { .pid = pid_u, .giorno = giorno_attuale, .minuto = minuto_attuale };
+        ElementoCoda e;
+        e.pid = pid_u;
+        e.giorno = giorno_att;
+        e.minuto = min_att;
+
         sem_wait_operation(sem_id, SEM_MUTEX, 1);
         int ok = accoda(&shm->code_servizio[indice], e);
-        if (ok == 0) shm->utenti_in_attesa++;
+        if (ok == 0) {
+            shm->utenti_in_attesa++;
+        }
         sem_post_operation(sem_id, SEM_MUTEX, 1);
 
         if (ok == 0) {
             sem_post_operation(sem_id, SEM_BASE_SERVIZI + indice, 1);
         } else {
-            fprintf(stderr, "[Erogatore] Coda servizio %d piena, PID %d scartato.\n", servizio, pid_u);
+            fprintf(stderr, "[Erogatore] Coda S%d PIENA! PID %d respinto.\n", servizio, pid_u);
             
             sem_wait_operation(sem_id, SEM_MUTEX, 1);
-            processa_utente_non_servito(shm, servizio);
+            shm->servizi_non_completati_oggi[indice]++;
+            shm->totale_servizi_non_completati[indice]++;
             sem_post_operation(sem_id, SEM_MUTEX, 1);
             
-            invia_risposta(msgid, (pid_t)pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_attuale);
+            invia_risposta(msgid, pid_u, ESITO_NON_SERVITO_GIORNO, servizio, giorno_att);
         }
     }
 
-    printf("[Erogatore] Terminazione in corso...\n");
     scollega_memoria_condivisa(shm);
-    printf("[Erogatore] Terminato.\n");
     return 0;
 }
